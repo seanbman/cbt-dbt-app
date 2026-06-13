694cdc499e6f049503fc5ec61e0a07db20c63cf4
seanbman <seannicholasburman@gmail.com>
2026-06-12 23:04:33 -0600

    docs: add local development instructions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./internal/views/**/*.templ", "./static/js/**/*.js"],
   theme: { extend: {} },
   plugins: [],
