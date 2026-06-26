95cb6d20b9f7c044e1e83a4e3cc1b2b78d2400e1
seanbman <seannicholasburman@gmail.com>
2026-06-26 11:32:08 -0600

    fix: use Vite regex alias for React Native Web

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      'react-native$': 'react-native-web',
-    },
+    alias: [
+      { find: /^react-native$/, replacement: 'react-native-web' },
+    ],
   },
   build: {
     outDir: 'dist',
