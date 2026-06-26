8e06fadc593a6c3053a7589da58459ee5339217f
seanbman <seannicholasburman@gmail.com>
2026-06-26 11:39:48 -0600

    fix: import AppRegistry from react-native-web

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native-web';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
